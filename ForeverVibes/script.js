da429f542db48f1228917a0f2da95bc7c978a2ea
boogiebaggins <skateyoung01@gmail.com>
2024-11-05 16:14:54 -0600

    Delete ForeverVibes directory

--- ForeverVibes/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    // This is where you'll add any JavaScript for your pages in the future.
-    console.log("Script loaded and ready.");
-});
-
-
