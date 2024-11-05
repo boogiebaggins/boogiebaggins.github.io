46ba44c22ac44f5700e619ceeeed268f9fa89629
boogiebaggins <skateyoung01@gmail.com>
2024-11-05 16:13:35 -0600

    Add files via upload

---
+++ ForeverVibes/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    // This is where you'll add any JavaScript for your pages in the future.
+    console.log("Script loaded and ready.");
+});
+
+
